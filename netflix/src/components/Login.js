2693676c974d2c2e5d1dc7b95c600d3730e3a1b0
Sameeksha Sharma <sameekshasharma017@gmail.com>
2026-07-28 21:08:06 +0530

    login continue

--- netflix/src/components/Login.js
+++ netflix/src/components/Login.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import Header from './Header'
-
+import { useState } from 'react'
 const Login = () => {
+  const [islogin,setIslogin]=useState(false);
+  const [fullname,setFullname]=useState("");
+   const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+  
+  const loginhandler=()=>{
+    setIslogin(!islogin);
+  }
+  const handledata=(e)=>{
+    e.preventDefault();
+    console.log(fullname,email,password);
+  }
   return (
     <div>
       <Header/>
       <div className='absolute'>
         < img className='w-[100vw] h-[100vh]'src="https://xboxwire.thesourcemediaassets.com/sites/2/2023/05/Background-size1920x1080-4e1694a6-75aa-4c36-9d4d-7fb6a3102005-bc5318781aad7f5c8520.png" alt="banner"/>
       </div>
-      <form className=' flex  flex-col w-3/12 my-36 p-12 left-0 right-0 mx-auto  items-center justify-center absolute bg-black opacity-90'>
+      <form  onSubmit={handledata}className=' flex  flex-col w-3/12 my-36 p-12 left-0 right-0 mx-auto  items-center justify-center absolute  rounded-md bg-black opacity-90'>
         
-        <h1 className='text-white text-3xl mb-5 font-bold'>Sign Up</h1>
+        <h1 className='text-white text-3xl mb-5 font-bold'>{islogin ?"Login":"SignUp"}</h1>
         <div className=' flex flex-col'>
              
-             <input type='text' placeholder='Full Name' className='outline-none p-3 my-2 rounded-md bg-gray-700 text-white'/>
-             <input type='text'placeholder='E-mail ID'className='outline-none p-3 my-2 rounded-md bg-gray-700 text-white'/>
-            <input type='password'placeholder='Password'className='outline-none p-3 my-2 rounded-md bg-gray-700 text-white'/>
-             <p>Already have an Account?</p>
+            {  !islogin&& <input value={fullname} onChange={e=>setFullname(e.target.value)}type='text' placeholder='Full Name'className='outline-none p-3 my-2 rounded-md bg-gray-700 text-white'/>}
+             <input value={email}  onChange={e=>setEmail(e.target.value)}type='text'placeholder='E-mail ID'className='outline-none p-3 my-2 rounded-md bg-gray-700 text-white'/>
+            <input  value={password} onChange={e=>setPassword(e.target.value)}type='password'placeholder ='Password'className='outline-none p-3 my-2 rounded-md bg-gray-700 text-white'/>
+             <button className='bg-red-600 rounded-md  text-white font-medium mt-8 p-3'>{islogin? "Login":"SignUp"}</button>
+             <p className='text-white mt-3  items-center justify-center'>{islogin?  "New to Netflix?" : "Already Have an Account?"}<span onClick={loginhandler} className='ml-1 text-blue-700 font-medium cursor-pointer'>{islogin? "SignUp":"Login"}</span></p>
         </div>
       </form>
     </div>
